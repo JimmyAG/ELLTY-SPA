8a75bded691e412742dc778c4468c700d56247e6
Jimmy Mohamed <jimmymoham10@gmail.com>
2025-11-08 21:01:01 +0200

    removed wrong next.config attributes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  compiler: {
-    removeConsole: true,
-  },
-  experimental: {
-    webpackBuildWorker: false, // Disable parallel workers
-  },
 }
 
 export default nextConfig
